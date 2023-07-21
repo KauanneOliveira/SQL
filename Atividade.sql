1b0c310a2dec0339f2128b8008642bf2d6db57c9
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-07-21 15:23:36 -0300

    Interação 3
    Criação da tabela Usuario.
    Utilização de Relacionamento 1 para N.

--- Atividade.sql
+++ Atividade.sql
@@ -1,19 +1,32 @@
-CREATE DATABASE RegistroT;
-USE RegistroT;
+CREATE DATABASE RegistroTarefa;
+USE RegistroTarefa;
 
 CREATE TABLE Tarefa(
     id INT PRIMARY KEY NOT NULL,
+    id_usuario INT NOT NULL,
     descricao VARCHAR(100) NOT NULL,
-    realizada BOOLEAN NOT NULL
+    realizada BOOLEAN NOT NULL,
+    FOREIGN KEY( id_usuario ) REFERENCES Usuario( id )
 );
 
-INSERT INTO Tarefa( id, descricao ) VALUES
-( 1, 'Comprar Carne Moida', false ),
-( 2, 'Limpar pia', false),
-( 3, 'Fazer lição de LG2', true),
-( 4, 'Arrumar a casa', true),
-( 5, 'Lavar a roupa', false),
-( 6, 'Andar 2 Km', true);
+CREATE TABLE Usuario(
+    id INT PRIMARY KEY NOT NULL,
+    nome VARCHAR( 100 ) NOT NULL,
+    email VARCHAR( 100 ) NOT NULL,
+    senha CHAR( 60 ) NOT NULL
+);
+
+INSERT INTO Usuario ( id, nome, email, senha ) VALUES
+( 1, 'João da Silva', 'joao@email.com', '*****' ),
+( 2, 'Maria da Silva', 'maria@email.com', '********' );
+
+INSERT INTO Tarefa( id, id_usuario, descricao, realizada ) VALUES
+( 1, 1, 'Comprar Carne Moida', false ),
+( 2, 1, 'Limpar pia', false),
+( 3, 2, 'Fazer lição de LG2', true),
+( 4, 1, 'Arrumar a casa', true),
+( 5, 2, 'Lavar a roupa', false),
+( 6, 2, 'Andar 2 Km', true);
 
 -- ALTER TABLE Tarefa ADD realizada BOOLEAN NOT NULL;
 
@@ -22,3 +35,5 @@ SET realizada = true
 WHERE id = 1;
 
 SELECT * FROM Tarefa;
+SELECT * FROM Usuario;
+SELECT * FROM Tarefa WHERE id_usuario = 2;
