70c119d30cee01568656bc81ee1459fe04d2daf8
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-07-21 17:03:40 -0300

    Comentários
    Comentando algumas linhas do código

--- Codigo_Sala.sql
+++ Codigo_Sala.sql
@@ -1,13 +1,16 @@
-CREATE DATABASE loja;
-USE loja;
+CREATE DATABASE loja; -- criou um banco de dados chamado loja
+USE loja; -- selecionou para usar
 
+-- criou tabela
 CREATE TABLE Produto(
-    codigo INT, 
+    -- atributos da tabela
+    codigo INT,   
     nome VARCHAR(100),
     preco NUMERIC(10,2),
     PRIMARY KEY(codigo)
 );
 
+-- inseriu nos atributos codigo, nome e preco valores 
 INSERT INTO Produto(codigo, nome, preco) VALUES
 (2, 'Água Mineral', 3.20),
 (3, 'Limão', 5.80),
@@ -15,10 +18,14 @@ INSERT INTO Produto(codigo, nome, preco) VALUES
 (5, 'Hambuguer', 12.90),
 (1, 'Coca-Cola', 5.40);
 
+-- update = atualizar, set = definir, where = onde
+-- mudou o preço do código 1
 UPDATE Produto
 SET preco=  5.0, nome= 'Coquinha'
 WHERE codigo=1;
 
+-- alterar table = alterar tabela, default = predefinição
+-- ta alterando a tabela e adicionando uma coluna chamado ativo do tipo booleana que não pode ser nulo
 ALTER TABLE Produto ADD ativo BOOLEAN NOT NULL DEFAULT true;
 
 UPDATE Produto
