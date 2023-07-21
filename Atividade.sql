44deecc16db46fd7b1272dbfad29a4f9fd5fd853
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-07-21 15:02:11 -0300

    Interação 2
    Foi criado o atributo "realizada" do tipo booleano, serve para dizer se a tarefa está feita ou não para o usuário.

--- Atividade.sql
+++ Atividade.sql
@@ -3,15 +3,22 @@ USE RegistroT;
 
 CREATE TABLE Tarefa(
     id INT PRIMARY KEY NOT NULL,
-    descricao VARCHAR(100) NOT NULL
+    descricao VARCHAR(100) NOT NULL,
+    realizada BOOLEAN NOT NULL
 );
 
 INSERT INTO Tarefa( id, descricao ) VALUES
-( 1, 'Comprar Carne Moida' ),
-( 2, 'Limpar pia'),
-( 3, 'Fazer lição de LG2'),
-( 4, 'Arrumar a casa'),
-( 5, 'Lavar a roupa'),
-( 6, 'Andar 2 Km');
+( 1, 'Comprar Carne Moida', false ),
+( 2, 'Limpar pia', false),
+( 3, 'Fazer lição de LG2', true),
+( 4, 'Arrumar a casa', true),
+( 5, 'Lavar a roupa', false),
+( 6, 'Andar 2 Km', true);
+
+-- ALTER TABLE Tarefa ADD realizada BOOLEAN NOT NULL;
+
+UPDATE Tarefa
+SET realizada = true
+WHERE id = 1;
 
 SELECT * FROM Tarefa;
